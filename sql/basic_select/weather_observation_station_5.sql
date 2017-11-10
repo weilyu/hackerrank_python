540bcc80b4744bcc79993da5c9dca568144fc1c0
weilyu <lvwei1001@live.com>
2017-11-10 22:44:59 +0800

    Weather Observation Station 5

---
+++ sql/basic_select/weather_observation_station_5.sql
@@ -0,0 +1,11 @@
+(select
+city, length(city) as len
+from station
+order by len asc, city asc
+limit 1)
+union
+(select
+city, length(city) as len
+from station
+order by len desc, city asc
+limit 1)
